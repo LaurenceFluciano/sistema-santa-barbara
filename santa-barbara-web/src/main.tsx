55782023b163696172a149c44ee0212e4b88a97b
LaurenceFluciano <laurencefluciano@gmail.com>
2026-08-30 18:12:54 -0300

    chore: adicionar script que efetua restrições arquiteturais: caminho relativo e dependencias invalidas.

--- santa-barbara-web/src/main.tsx
+++ santa-barbara-web/src/main.tsx
@@ -1,8 +1,8 @@
+import App from '@/App';
+import '@/index.css';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter, Route, Routes } from "react-router";
-import App from './App.tsx';
-import './index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
--- santa-barbara-web/vite.config.ts
+++ santa-barbara-web/vite.config.ts
@@ -1,5 +1,6 @@
 import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
+import path from 'node:path'
 import { defineConfig } from 'vite'
 
 // https://vite.dev/config/
@@ -10,5 +11,12 @@ export default defineConfig({
   ],
   build: {
     sourcemap: false
+  },
+
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
   }
+
 })
