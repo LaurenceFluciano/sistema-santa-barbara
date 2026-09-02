5551e565c7bd032565eaa169018e5b1405beba3f
LaurenceFluciano <laurencefluciano@gmail.com>
2026-09-02 17:00:29 -0300

    Tornar tela de login responsiva com desktop e dispositivos mobiles.

--- santa-barbara-web/src/application/pages/Login.tsx
+++ santa-barbara-web/src/application/pages/Login.tsx
@@ -8,35 +8,40 @@ function Login() {
         <form 
             action="" 
             className={`
-                max-w-[744px]
-                w-[50%]
-                min-w-[500px]
+                w-[90%]
+                xl:w-[35%]
+                h-fit
+
                 border-1 border-solid border-[var(--light-neutral-color)]
-                px-[72px] pb-[64px] pt-[36px]
-                mx-auto
+                
+                2xl:px-[72px] 2xl:pb-[64px] 2xl:pt-[36px]
+                xl:px-[48px] xl:pb-[32px] xl:pt-[18px]
+                px-[32px] pb-[16px] pt-[9px]
+                
                 rounded-[16px]
-                mt-32
-                mb-32
+                
+                mx-auto
+                my-auto
+                gap-[24px]
 
                 flex
                 flex-col
-                gap-[24px]
             `}
         >
         
-            <h1 className="py-[32px] mx-auto login__title">Login</h1>
+            <h1 className="2xl:py-[32px] py-[16px] mx-auto login__title">Login</h1>
 
-            <div className="flex flex-col gap-[12px]">
+            <div className="flex flex-col 2xl:gap-[12px] gap-[6px]">
                 <label htmlFor="username" className="login__label">Nome de usuário ou Email:</label>
                 <Input name="username" variant="normal" placeholder="Email ou Nome de Usuário" />
             </div>
 
-            <div className="flex flex-col gap-[12px]">
+            <div className="flex flex-col 2xl:gap-[12px] gap-[6px]">
                 <label htmlFor="password" className="login__label">Senha:</label>
                 <Input name="password" variant="normal" placeholder="Senha" />
             </div>
 
-            <div className="flex flex-row w-[100%] pt-[64px]">
+            <div className="flex flex-row w-[100%] 2xl:pt-[64px] pt-[24px]">
                 <Button type="submit" className="w-[100%]">Entrar</Button>
             </div>
                 
