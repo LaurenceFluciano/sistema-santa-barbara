3d285d0ed41ecb24f453ddd78603a4ff1171b728
LaurenceFluciano <laurencefluciano@gmail.com>
2026-09-04 16:40:38 -0300

    Adicionar repositorio para gerenciar existencia de membro e persistir os dados do admin.

--- santa-barbara-api/bandasantabarbara/src/main/java/br/org/bandasantabarbara/repositories/MembroRepository.java
+++ santa-barbara-api/bandasantabarbara/src/main/java/br/org/bandasantabarbara/repositories/MembroRepository.java
@@ -1,4 +1,24 @@
 package br.org.bandasantabarbara.repositories;
 
-public interface MembroRepository {
+import br.org.bandasantabarbara.model.Membro;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.Repository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+public interface MembroRepository extends Repository<Membro, UUID> {
+
+    Membro save(Membro membro);
+
+    Optional<Membro> findById(UUID id);
+
+
+    @Query("""
+        SELECT CASE WHEN COUNT(m) > 0 THEN true ELSE false END 
+        FROM Membro m 
+        JOIN m.papeis p 
+        WHERE p.nome = 'SUPER_ADMIN'
+    """)
+    boolean existeAdminCadastrado();
 }
--- santa-barbara-api/bandasantabarbara/src/main/java/br/org/bandasantabarbara/repositories/SetupApplicationRepository.java
+++ santa-barbara-api/bandasantabarbara/src/main/java/br/org/bandasantabarbara/repositories/SetupApplicationRepository.java
@@ -1,4 +1,13 @@
 package br.org.bandasantabarbara.repositories;
 
+import br.org.bandasantabarbara.model.Membro;
+
+import java.util.UUID;
+
 public interface SetupApplicationRepository {
+    void salvarAdmin(
+            Membro membro
+    );
+
 }
+
